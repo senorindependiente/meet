ec7c2736e1d4e0aa77902c9252868109383db306
senorindependiente <pingelchristian@hotmail.com>
2022-03-20 13:52:13 +0100

    Adding CitySearch component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
-import EventList from './EventList';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
 class App extends Component {
   render() {
     return (
       <div className="App">
-<EventList />
+        <CitySearch />
+        <EventList />
       </div>
     );
   }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-import EventList from "../EventList"
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+import EventList from "../EventList";
+import CitySearch from "../CitySearch"
 
-describe('<App /> component', () => {
-test("render list of events", ()=>{
-    const AppWrapper = shallow(<App/>);
+describe("<App /> component", () => {
+  test("render list of events", () => {
+    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
-})
+  });
+
+  test("render CitySearch", () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 });
