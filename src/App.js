d75e6c6d882ecfca5759f02d90314c410e7cc6b8
senorindependiente <pingelchristian@hotmail.com>
2022-04-04 15:32:43 +0200

    Adding Alert

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 import "./nprogress.css";
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-} from "recharts";
-import { StatusAlert } from "./Alert";
+// import { StatusAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
@@ -24,34 +16,20 @@ class App extends Component {
     showWelcomeScreen: undefined,
   };
 
-  getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter(
-        (event) => event.location === location
-      ).length;
-      const city = location.split(", ").shift();
-      return { city, number };
-    });
-    return data;
-  };
-
   async componentDidMount() {
-    if (navigator.onLine && !window.location.href.startsWith('http://localhost')) {
-      const accessToken = localStorage.getItem("access_token");
-      const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-      const searchParams = new URLSearchParams(window.location.search);
+    this.mounted = true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
 
-      const code = searchParams.get("code");
-
-      this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-      if ((code || isTokenValid) && this.mounted) {
-        getEvents().then((events) => {
-          if (this.mounted) {
-            this.setState({ events, locations: extractLocations(events) });
-          }
-        });
-      }
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
     }
   }
 
@@ -78,60 +56,35 @@ class App extends Component {
           events: locationEvents.slice(0, this.state.numberOfEvents),
           currentLocation: location,
           numberOfEvents: eventCount,
-          infoText: "You are offline."
         });
       }
     });
   };
 
-
   render() {
-    if (this.state.showWelcomeScreen === undefined && navigator.onLine) {
+    if (this.state.showWelcomeScreen === undefined)
       return <div className="App" />;
-    }
-    if (this.state.showWelcomeScreen === true)
 
-      return (
-        <div className="App">
-        
-            <StatusAlert text={this.state.infoText} />
-          
-          <CitySearch
-            locations={this.state.locations}
-            updateEvents={this.updateEvents}
-          />
-          <NumberOfEvents updateNumberOfEvents={this.updateNumberOfEvents} />
-          <h4>Events in each city</h4>
-
-          <ScatterChart
-            width={400}
-            height={400}
-            margin={{
-              top: 20,
-              right: 20,
-              bottom: 20,
-              left: 20,
-            }}
-          >
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" />
-            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-
-          <EventList
-            events={this.state.events}
-            numberOfEvents={this.state.numberOfEvents}
-          />
-          <WelcomeScreen
+    return (
+      <div className="App">
+        {/* <StatusAlert text={this.state.infoText} /> */}
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents updateNumberOfEvents={this.updateNumberOfEvents} />
+        <EventList
+          events={this.state.events}
+          numberOfEvents={this.state.numberOfEvents}
+        />
+        <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
             getAccessToken();
           }}
         />
-        </div>
-      );
+      </div>
+    );
   }
 }
 
