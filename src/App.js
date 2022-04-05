59ad4fcbaca8b6feb5b78bb20212faf8327f5d96
senorindependiente <pingelchristian@hotmail.com>
2022-04-05 16:17:13 +0200

    Adding styling

--- src/App.js
+++ src/App.js
@@ -107,8 +107,8 @@ class App extends Component {
         />
 
         <div className="data-vis-wrapper">
-          <EventGenre events={events}  />
-          <ResponsiveContainer height={400}>
+          <EventGenre className= "charts" events={events}  />
+          <ResponsiveContainer  height={400}>
             <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
               <CartesianGrid />
               <XAxis type="category" dataKey="city" name="city" />
